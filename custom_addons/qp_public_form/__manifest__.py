f4e904ba596d7438fa30bd15877161c27e1bcc1a
harisshah-cmd <haris_shah@flatratepunching.com>
2026-04-13 17:25:02 +0500

    Add qp_public_form manifest

---
+++ custom_addons/qp_public_form/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "QP Public Form",
+    "version": "1.0",
+    "summary": "Public website form for Patch Records",
+    "category": "Website",
+    "depends": ["website"],
+    "data": [
+        "views/public_form_templates.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
